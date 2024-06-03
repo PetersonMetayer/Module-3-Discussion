df80a876d7662f5c885ed7811bc07f9e4a586930
PetersonMetayer <168232989+PetersonMetayer@users.noreply.github.com>
2024-06-03 10:09:18 -0400

    Add files via upload

---
+++ Discussion3.cs
@@ -0,0 +1,18 @@
+using system;
+
+module_3_Discussion
+{
+   class program
+   {
+       class Octopus
+       {
+       string name;
+       public int age = 20;
+       static readonly int legs = 8, eyes = 2;
+       {
+       static void Main()
+       {
+       TopLevel.Color color = TopLevel.Color.Orange;
+       }
+       }
+       Fields - readonly
